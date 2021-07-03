52d6d6e08ac1b9955abf2526b2523020e0d6eec8
Peter Leiva <85527528+peterleiva@users.noreply.github.com>
2021-07-02 23:43:39 -0300

    feat: WIP: compress body response

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from "express";
 import createError from "http-errors";
 import type { Request, Response } from "express";
 import config from "config";
+import compression from "compression";
 
 const app = express();
 
 app.set("x-powered-by", config.env("dev"));
 app.set("env", config.env("production") ? "production" : config.env());
+app.use(compression());
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Olá, mundo");
