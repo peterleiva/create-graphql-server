19e5c39781d48faddb89e260f43ca2cb6704317a
Peter Leiva <85527528+peterleiva@users.noreply.github.com>
2021-08-05 20:03:08 -0300

    build: enhance webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
+const { BannerPlugin } = require("webpack");
 const { organizer } = require("webpack-config-organizer");
 
 /**
@@ -7,23 +8,29 @@ const { organizer } = require("webpack-config-organizer");
  * @param {boolean} [env.clean = false]
  * @return {import("webpack").Configuration}
  */
-module.exports = organizer(
-	["typescript", "babel", "resolver"],
-	({ clean = false }) => ({
-		target: "node",
-		externalsPresets: { node: true },
-		externals: [nodeExternals()],
+module.exports = organizer(["typescript", "babel", "resolver"], () => ({
+	target: "node",
+	externalsPresets: { node: true },
+	externals: [nodeExternals()],
 
-		context: path.resolve(__dirname, "src"),
-		entry: "./main.ts",
-		output: {
-			filename: "[name].js",
-			path: path.resolve(__dirname, "dist"),
-			clean,
-		},
+	context: path.resolve(__dirname, "src"),
+	entry: "main.ts",
+	output: {
+		filename: "[name].js",
+		path: path.resolve(__dirname, "dist"),
+	},
 
-		optimization: {
-			runtimeChunk: "single",
-		},
-	})
-);
+	plugins: [
+		new BannerPlugin({
+			banner: "#!/usr/bin/env node",
+			raw: true,
+			entryOnly: true,
+		}),
+	],
+
+	optimization: {
+		runtimeChunk: "single",
+		moduleIds: "deterministic",
+		chunkIds: "named",
+	},
+}));
