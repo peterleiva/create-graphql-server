18add4bc40010c25db243aa4e2182fdea8077f67
Peter <85527528+pherval@users.noreply.github.com>
2021-08-08 18:26:32 -0300

    feat: add logger for running server

---
+++ src/server/handlers/running-logger.ts
@@ -0,0 +1,45 @@
+import type { Config } from "config";
+import { sprintf } from "sprintf-js";
+import styleAddress from "../style-address";
+import address from "address";
+import chalk from "chalk";
+
+/**
+ * Inform to the user that the server is running. print the message to shell
+ *
+ * @param config
+ * @returns
+ */
+export default function runningLogger(config: Config): () => void {
+	const port = config.port;
+
+	return () => {
+		console.info(
+			sprintf(
+				"%5s %s %s",
+				"🚀",
+				`Server is ${chalk.green("running").toLowerCase()}`,
+				`on 📦 ${chalk.magenta(config.env())} environment`
+			)
+		);
+
+		if (config.env("dev")) {
+			console.info(
+				sprintf(
+					"%5s %-18s%20s",
+					"🔈",
+					"Listening on",
+					styleAddress({ hostname: "localhost", port })
+				)
+			);
+			console.info(
+				sprintf(
+					"%5s  %-18s%20s",
+					"🕸",
+					"On your network",
+					styleAddress({ hostname: address.ip(), port })
+				)
+			);
+		}
+	};
+}
