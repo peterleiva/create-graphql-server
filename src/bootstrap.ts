41f5d9446458d77b1958397f07b3bbf63fab3ac6
Peter <github@peter.dev.br>
2021-08-17 10:40:25 -0300

    move bootstrap to main and rename server-manager

--- src/bootstrap.ts
+++
@@ -1,8 +0,0 @@
-import { createServer } from "server";
-import app from "app";
-import config from "config";
-
-export default function bootstrap(): void {
-	const www = createServer(app, config);
-	www.start();
-}
